41b6f8136238273f8d4b745e87669ff2a97dc7e3
Nolan Neilson <NeilNola532@GoTVDSB.ca>
2021-02-19 12:37:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Part_5___Decisions
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            int userAge;
+
+
+            ///numeric age check
+            Console.WriteLine("Please enter your age: ");
+            while (!Int32.TryParse(Console.ReadLine(), out userAge))
+            {
+                Console.WriteLine("Please enter a valid age");
+            }
+
+            ///age grouping
+            if (userAge <= 5)
+            {
+                Console.WriteLine("you are a toddler. ");
+            }
+            else if (userAge <= 10)
+            {
+                Console.WriteLine("you are a child. ");
+            }
+            else if (userAge <= 12)
+            {
+                Console.WriteLine("you are a preteen. ");
+            }
+            else if (userAge > 18)
+            {
+                Console.WriteLine("you are an adult. ");
+            }
+            else if (userAge > 12)
+            {
+                Console.WriteLine("you are a teenager. ");
+            }
+               
+
+            Console.ReadLine();
+        }
+    }
+}
